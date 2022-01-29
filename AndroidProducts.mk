a667a63fe0b5ce51f676e1908d24e1adc27549d7
iskandarasri03 <71484902+iskandarasri03@users.noreply.github.com>
2022-01-29 09:13:34 +0800

    Arrow OS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/nad_garden.mk
+   $(LOCAL_DIR)/arrow_garden.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_garden-userdebug \
-    nad_garden-eng \
-    nad_garden-user
+    arrow_garden-userdebug \
+    arrow_garden-eng \
+    arrow_garden-user
