c8b30726e1cf5d6e48e25c33ea092803b938a067
iskandarasri03 <71484902+iskandarasri03@users.noreply.github.com>
2022-02-02 18:12:38 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/lineage_garden.mk
+   $(LOCAL_DIR)/carbon_garden.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_garden-userdebug \
-    lineage_garden-eng \
-    lineage_garden-user
+    carbon_garden-userdebug \
+    carbon_garden-eng \
+    carbon_garden-user
